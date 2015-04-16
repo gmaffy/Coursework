30963498673df623595c0395b099c01f377b5300
philnova <pnova8@gmail.com>
2015-04-15 17:38:02 -0700

    Corrected b-f algorithm

--- StanfordAlgDesign2/week4_allpairsshortestpath.py
+++ StanfordAlgDesign2/week4_allpairsshortestpath.py
@@ -14,7 +14,7 @@ def graph_reader(filename):
 				n_nodes = int(line.split()[0])
 				out_edge_dict = {i:[] for i in range(1,n_nodes+1)} #nodes are indexed from 1 #hash table of tail node : [(head node, edge weight) ... ]
 				in_edge_dict = {i:[] for i in range(1,n_nodes+1)} #hash table of head node : [(tail node, edge weight) ... ]
-				cheapest_incoming_edge = {i: (None, float("inf")) for i in range(1,n_nodes)} #hash table of head node : (min-cost tail node, edge weight)
+				cheapest_incoming_edge = {i: (None, float("inf")) for i in range(1,n_nodes+1)} #hash table of head node : (min-cost tail node, edge weight)
 			else:
 				tail, head, cost = [int(i) for i in line.split()]
 	
@@ -35,10 +35,13 @@ def bellman_ford(directed_graph, source):
 		A[0][v] = float("inf")
 	A[0][source - 1] = 0
 
-	for i in range(directed_graph.n_nodes):
+	for i in range(1,directed_graph.n_nodes):
 		for v in range(directed_graph.n_nodes):
-			cheapest_head, cheapest_cost = directed_graph.cheapest_in_edges[v+1]
-			A[i][v] = min(A[i-1][v], A[i-1][cheapest_head - 1] + cheapest_cost)
+				cheapest_head, cheapest_cost = directed_graph.cheapest_in_edges[v+1]
+				possibilities = directed_graph.in_edges[v+1] #list of head, cost incident to v+1
+				possible_costs = [A[i-1][poss[0]-1] + poss[1] for poss in possibilities]
+				possible_costs.append(A[i-1][v])
+				A[i][v] = min(possible_costs)
 
 	#detect negative-cost cycles
 	for v in range(directed_graph.n_nodes):
@@ -46,3 +49,7 @@ def bellman_ford(directed_graph, source):
 			return "Problem is intractable due to negative-cost cycles"
 	return A
 
+## test ##
+dg = DirectedGraph(*graph_reader("g3.txt"))
+bf = bellman_ford(dg,1)
+print bf[600]
