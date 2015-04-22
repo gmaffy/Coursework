125e590a5e89e534029949f4c646aa6d770a7d15
philnova <pnova8@gmail.com>
2015-04-22 15:18:24 -0700

    Making progress using binary strings
    Need to decide which rows of the array are necessary at any given time
    -- is the # of subproblems the important constraint on this?

--- StanfordAlgDesign2/week5_travelingsalesman.py
+++ StanfordAlgDesign2/week5_travelingsalesman.py
@@ -1,4 +1,9 @@
 import math
+import itertools
+
+def all_binary_strings(length):
+	#print sorted(["".join(seq) for seq in itertools.product("01", repeat=length)])
+	return ["".join(seq) for seq in itertools.product("01", repeat=length)]
 
 def subsets(mySet):
 	"""Take a list of elements and return all permutations of that list,
@@ -30,6 +35,7 @@ class TravelingSalesman(object):
 	
 	def build_subsets(self):
 		self.subset_to_integer = {tuple(sorted(item+[0])) : idx for idx, item in enumerate(subsets([i for i in range(1,self.n_nodes)]))}
+
 		self.integer_to_subset = dict((v, k) for (k, v) in self.subset_to_integer.items())
 
 		# self.subsets_containing_node = {i : [] for i in range(self.n_nodes)} #allows us to know which subsets contain given nodes
@@ -67,14 +73,101 @@ class TravelingSalesman(object):
 		#return self.array[final_subset_id]
 		return min([self.array[final_subset_id][j] + self.cost(j,0) for j in range(2,self.n_nodes)])
 
+class TravelingSalesmanBinary(TravelingSalesman):
 
-testgraph_linear = {i : [0,i] for i in range(5)}
+	def build_subsets(self):
+		#self.subset_to_integer = {'1'+item[::-1] : int(item,2) for item in all_binary_strings(self.n_nodes - 1)}
+		self.subset_array = []
+		formatting = '{0:'+str(self.n_nodes-1)+'b}'
+		print 2**(self.n_nodes-1)
+		for int10 in range(2**(self.n_nodes - 1)):
+			bin = formatting.format(int10)
+			if len(bin.split()[0]) < self.n_nodes - 1:
+				bin = '0'*(self.n_nodes - 1 - len(bin.split()[0])) + bin.split()[0]
+			self.subset_array.append('1'+bin[::-1])
+
+		#print self.subset_array[0:100]
+		#self.integer_to_subset = dict((v, k) for (k, v) in self.subset_to_integer.items())
+		self.subsets_by_size = {i : [] for i in range(1,self.n_nodes+1)} #maps size of subsets : integer IDs of subsets of that size
+		for subset in self.subset_array:
+			#print subset, subset.count("1"), self.subset_to_index(subset)
+			self.subsets_by_size[subset.count("1")].append(self.subset_to_index(subset))
+
+		#print self.subset_array
+		#print self.subsets_by_size
+
+	def subset_to_index(self, subset):
+		binary = subset[1::]
+		binary = binary[::-1]
+		integer = int(binary, 2)
+		#print 'binint', binary, integer
+		return integer
+
+	def build_array(self):
+		#A will be a 2D array indexed by subsets of nodes that contain 0, and destinations
+		#j = {1,2,...,n}
+		#self.array = [[None for dummy1 in range(self.n_nodes)] for dummy2 in range(len(self.subset_array))]
+		self.array = [[None for dummy1 in range(self.n_nodes)] for dummy2 in range(2)]
+		for row_index in range(2):
+			self.array[row_index][0] = float("inf")
+		self.array[self.subset_to_index(("1"+"0"*(self.n_nodes -1)))][0] = 0
+		print self.array
+
+	def solve(self):
+		for subproblem_size in range(2,self.n_nodes+1):
+			print subproblem_size, len(self.subsets_by_size[subproblem_size])
+			for subset_id in self.subsets_by_size[subproblem_size]:
+				#print subset_id
+				destination = self.subset_array[subset_id]
+
+				if not destination == "1"+"0"*(self.n_nodes - 1):
+
+					for dest_idx, bit in enumerate(destination):
+						if int(bit) and not dest_idx == 0:
+							subset_without_dest = destination[0:dest_idx]+"0"+destination[dest_idx+1::]
+							subsetid_without_dest = self.subset_to_index(subset_without_dest)
+							#print subset_without_dest
+							#print subset_id
+							#print dest_idx
+
+							recurrence_array = []
+							for idx, k in enumerate(subset_without_dest):
+								if int(k) and not idx == dest_idx:
+									#print idx, dest_idx
+									#print subsetid_without_dest
+									#print destination, subset_without_dest
+									#print self.array[subsetid_without_dest]
+									recurrence_array.append(self.array[subsetid_without_dest][idx] + self.cost(idx, dest_idx))
+
+
+							#recurrence_array = [self.array[subsetid_without_dest][idx] + self.cost(idx, dest_idx) for idx, k in enumerate(subset_without_dest) if int(k) and not idx == dest_idx]
+							#self.array[subset_id][dest_idx] = min(recurrence_array)
+							#self.array[1][dest_idx] = min(recurrence_array)
+
+				#dump the first row of the array, add a new one
+				self.array = [self.array[1],[None for i in range(self.n_nodes)]]
+				self.array[1][0] = float("inf")
+
+		#return self.array
+		final_subset_id = self.subsets_by_size[self.n_nodes][0]
+		#return self.array[final_subset_id]
+		return min([self.array[final_subset_id][j] + self.cost(j,0) for j in range(2,self.n_nodes)])
+
+
+testgraph_rect = {i : [0,i] for i in range(5)}
 for j in range(9,4,-1):
-	testgraph_linear[j] = [1,j-5]
+	testgraph_rect[j] = [1,j-5]
+print testgraph_rect
+
+testgraph_linear = {i: [0,i] for i in range(4)}
 print testgraph_linear
 
 if __name__ == "__main__":
+	#x = sorted(all_binary_strings(24))
+	#subset_to_integer = {'1'+item[::-1] : int(item,2) for item in all_binary_strings(25 - 1)}
+	#print x[0:100]
 	graph = ts_reader("tsp.txt")
-	ts = TravelingSalesman(testgraph_linear)
-	print "dictionarieis built"
+	#ts = TravelingSalesmanBinary(testgraph_rect)
+	ts = TravelingSalesmanBinary(testgraph_rect)
+	print "data structures built"
 	print ts.solve()
